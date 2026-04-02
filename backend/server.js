2b9cdfe39d699d255d428efb6f053996fbcd3960
Wilson Oyem <85078140+oyemwilson@users.noreply.github.com>
2026-04-02 14:57:33 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(cors({
   origin: [
     'http://localhost:3000',
-    'https://vune.onrender.com'
+    'https://vune-1.onrender.com'
   ],
   credentials: true,
 }));
